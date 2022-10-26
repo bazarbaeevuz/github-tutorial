5b55a97ca09b05ca34250743f695992b96e7b207
Bobur <@bazarbaeevuz@gmail.com>
2022-10-26 15:03:14 +0300

    first commit

---
+++ Workjs/code/Js/js/script.js
@@ -0,0 +1,22 @@
+
+// let number = 10;
+// const borderColor = "red";
+// console.log(number);
+
+// number = 20;
+// console.log(number);
+
+// const person = {
+//     name: "Bobur",
+// };
+// person.name = "Samar";
+// console.log(person);
+
+// // Old version
+
+// console.log(clientName);
+// var clientName = "Bobur"; // var da xatolar xam underfined ko'rinishida bo'lishi mumkin
+
+//Interpolatsiya
+
+//  
---
+++ Workjs/code/Js/js/strict.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//alert("Hello World");
+//const place = confirm("Are you here?");
+//console.log(place);
+
